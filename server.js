d3ffcec538c0b87baa876e6bc211d7af2924504d
HemwantPrasad <hemwant@gmail.com>
2017-01-14 18:32:16 +0530

    intialize repo

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var PORT =3000;
+
+
+
+app.use(express.static(__dirname+'/public'));
+app.listen(PORT, function() {console.log('server satrted at port: ' + PORT)});
