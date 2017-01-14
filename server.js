cac015026d81e4b9a729c0465bcf3f0acd8eec54
HemwantPrasad <hemwant@gmail.com>
2017-01-14 18:40:27 +0530

    added middleware logs

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ var express = require('express');
 var app = express();
 var PORT =3000;
 
+var middleware ={
+    requireAuthentication: function(req, res, next) {
+        console.log('Private route hit!')
+        next();
+    },
+    logger: function(req, res, next) {
+        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+     } 
+};
 
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+    res.send(' ABOUT US !!');
+});
 
 app.use(express.static(__dirname+'/public'));
 app.listen(PORT, function() {console.log('server satrted at port: ' + PORT)});
